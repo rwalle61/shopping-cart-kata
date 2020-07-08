5e2550f9233895ef00a35185b2d04d90b077a042
Richard Waller <Richard.Waller@ibm.com>
2020-07-08 16:18:24 +0100

    test, feat: show default headings

--- src/pages/index.test.tsx
+++ src/pages/index.test.tsx
@@ -3,8 +3,19 @@ import { render, screen } from '@testing-library/react';
 import Home from '.';
 
 describe('Home page', () => {
-  it('renders products', () => {
+  beforeEach(() => {
     render(<Home />);
-    expect(screen.getByText('Products')).toBeInTheDocument();
+  });
+  describe('when app starts', () => {
+    it('renders the "Products" title', () => {
+      expect(screen.getByText('Products')).toBeInTheDocument();
+    });
+    it('renders the "Cart" title', () => {
+      expect(screen.getByText('Cart')).toBeInTheDocument();
+    });
+    it('renders the default basket price (£0.00)', () => {
+      expect(screen.getByText('Total:')).toBeInTheDocument();
+      expect(screen.getByText('£0.00')).toBeInTheDocument();
+    });
   });
 });
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,10 @@
-const Home = (): JSX.Element => <div>Products</div>;
+const Home = (): JSX.Element => (
+  <div>
+    <div>Products</div>
+    <div>Cart</div>
+    <div>Total:</div>
+    <div>£0.00</div>
+  </div>
+);
 
 export default Home;
